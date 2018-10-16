284fec841b3c0fa08f24680ee3625c36ef19fc3d
张海锋 <hooverz@foxmail.com>
2018-10-16 23:42:31 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,106 @@
+//index.js
+
+import Toast from '../../dist/toast/toast';
+
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    movies: [
+      { url: 'http://img3.imgtn.bdimg.com/it/u=1021379568,1897520673&fm=200&gp=0.jpg' },
+      { url: 'http://img4.imgtn.bdimg.com/it/u=2963399691,2477161139&fm=26&gp=0.jpg' },
+      { url: 'http://img3.imgtn.bdimg.com/it/u=3944340496,3864633243&fm=200&gp=0.jpg' },
+      { url: 'http://img2.imgtn.bdimg.com/it/u=3073478656,1928812130&fm=200&gp=0.jpg' }
+    ],
+    grids: [
+      {
+        name: '海鸭蛋',
+        url: '/pages/Course/course',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '10'
+      },
+      {
+        name: '柚子',
+        url: '/pages/Course/course',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '11'
+      },
+      {
+        name: '百香果',
+        url: '/pages/Course/course',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '10'
+      },
+      {
+        name: '蛋黄酥',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '11'
+      },
+      {
+        name: '茶叶',
+        url: '/pages/Course/course',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '10'
+      },
+      {
+        name: '石榴',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '11'
+      },
+      {
+        name: '芭乐',
+        url: '/pages/Course/course',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '10'
+      },
+      {
+        name: '雪莲果',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '11'
+      },
+      {
+        name: '芒果',
+        url: '/pages/Course/course',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '10'
+      },
+      {
+        name: '其他',
+        url: '/pages/Course/course',
+        icon: 'http://img5.imgtn.bdimg.com/it/u=3980755989,828156708&fm=26&gp=0.jpg',
+        code: '10'
+      }
+    ],
+    swiperCurrent: 0,
+    active: 1
+  },
+  onLoad: function () {
+
+  },
+  onPullDownRefresh: function () {
+
+  },
+  onReachBottom: function () {
+
+  },
+  //轮播图的切换事件
+  swiperChange: function (e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  },
+  //轮播图片点击事件
+  swiperclick: function (e) {
+    console.log(this.data.swiperCurrent)
+    Toast("点击位置: " + this.data.swiperCurrent);
+  },
+  //Tab页面切换
+  onTabChange(e) {
+    console.log(JSON.stringify(e))
+    wx.showToast({
+      title: `切换到标签 ${event.detail.index + 1}`,
+      icon: 'none'
+    });
+  }
+})
